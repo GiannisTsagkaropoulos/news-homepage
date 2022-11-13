bceeaf01cdcc30562de16ef0653b0f9ddc175f00
Giannis Tsagkaropoulos <johntsagas2003@gmail.com>
2022-11-14 01:22:49 +0200

    remove burger photo and old effect, add new one and have main functionality working

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
-
-// function myFunction() {
-//     var x = document.getElementsByClassName("my-link");
-//     for (item of x) {
-//         if ((item.classList).contains('hide')) {
-//             item.classList.remove('hide')
-//         } else {
-//             item.classList.add('hide')
-//         }
-//     }
-// }
-
+let isPressed = false;
 $(window).resize(function () {
-    var links = document.getElementsByClassName("my-link");
-    var burger = document.getElementById('burger');
-    console.dir(burger)
-    if ($(window).width() < 800) {
-        for (link of links) {
-            $(link).addClass('hide');
-        }
-    } else {
-        for (link of links) {
-            $(link).removeClass('hide');
-        }
+    console.log(isPressed)
+    if ($(window).width() > 900 && isPressed == true) {//to z index einai 1 ara to kanw -1
+        $(".div-class").removeClass("toggle-burger");
+    }
+    if ($(window).width() < 900 && isPressed == true) {
+        $(".div-class").addClass("toggle-burger");
     }
 }).resize();
 
 
 $('.menu-toggle').click(function () {
+    // console.log(isPressed)
     $(".nav").toggleClass("mobile-nav");
     $(this).toggleClass("is-active");
     $("ul").toggleClass("test");
     $(".div-class ").toggleClass("toggle-burger");
     $(".nav-wrapper").toggleClass("zind");
-    // $(".menu-toggle").toggleClass("test");
+    if ($(".div-class").hasClass("toggle-burger")) {
+        isPressed = true;
+    } else {
+        isPressed = false;
+    }
 });
