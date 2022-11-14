b3cd3f7b7775cc10f3501f6583822d97c1dccc3c
Giannis Tsagkaropoulos <johntsagas2003@gmail.com>
2022-11-14 14:55:00 +0200

    fix navbar animation and set media query for phone screen

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 let isPressed = false;
+
+$('.menu-toggle').click(function () {
+    // console.log(isPressed)
+    $(".nav").toggleClass("mobile-nav");
+    $(this).toggleClass("is-active");
+    $(".div-class ").toggleClass("toggle-burger");
+    $(".nav-wrapper").toggleClass("zind");
+    if ($(".div-class").hasClass("toggle-burger")) {
+        isPressed = true;
+        $(".no-search").removeClass(".none-disp")
+    } else {
+        isPressed = false;
+    }
+});
+
 $(window).resize(function () {
     console.log(isPressed)
     if ($(window).width() > 900 && isPressed == true) {//to z index einai 1 ara to kanw -1
@@ -10,16 +25,3 @@ $(window).resize(function () {
 }).resize();
 
 
-$('.menu-toggle').click(function () {
-    // console.log(isPressed)
-    $(".nav").toggleClass("mobile-nav");
-    $(this).toggleClass("is-active");
-    $("ul").toggleClass("test");
-    $(".div-class ").toggleClass("toggle-burger");
-    $(".nav-wrapper").toggleClass("zind");
-    if ($(".div-class").hasClass("toggle-burger")) {
-        isPressed = true;
-    } else {
-        isPressed = false;
-    }
-});
